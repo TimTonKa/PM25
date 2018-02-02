01e8bf3fe3321bd3d94dc041e5bece184b30b27d
Tim <tim@gomaji.com>
2018-02-02 16:37:16 +0800

    Crashlytics finished

--- PM25/ViewController.swift
+++ PM25/ViewController.swift
@@ -7,13 +7,54 @@
 //
 
 import UIKit
+import Crashlytics
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var dataArray = [String]()
+    var count = 0
+    
+    let defaultCellID = "defaultCellID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: defaultCellID)
+        
+        dataArray.append("one")
+        dataArray.append("two")
+        dataArray.append("three")
+        dataArray.append("four")
     }
+    
+    //MARK: UITableViewDelegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: defaultCellID, for: indexPath)
+        cell.textLabel?.text = dataArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print("number of cell\(indexPath.row) pressed!!")
+        self.testCrash()
+    }
+    
+    func testCrash() {
+        let counts = 5/count
+        print(counts)
+    }
+    
+    //MARK: UITableViewDelegate
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
